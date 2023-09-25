554f0c4190458cc11caea9f49468f1da60492d07
EdvardDatser <120181268+EdvardDatser@users.noreply.github.com>
2023-09-25 10:29:38 +0300

    Index AdventureDB.sql

--- adventureDB.sql
+++ adventureDB.sql
@@ -91,3 +91,68 @@ CREATE index IX_DimEmployee_lHours
 ON DimEmployee(SickLeaveHours ASC)
 
 Drop index DimEmployee.NameIndex
+
+
+--Пятый файл
+
+CREATE TABLE [tblEmployee]
+(
+id int Primary key,
+Name nvarchar(50),
+Salary int,
+Gender nvarchar(10),
+City nvarchar(50));
+
+INSERT INTO tblEmployee
+Values(3,'John',4500,'Male','New York');
+INSERT INTO tblEmployee
+Values(1,'Mike',2500,'Male','London');
+INSERT INTO tblEmployee
+Values(4,'Sara',5500,'Female','Tokyo');
+INSERT INTO tblEmployee
+Values(5,'Todd',3100,'Male','Toronto');
+INSERT INTO tblEmployee
+Values(2,'Pam',6500,'Female','Sydney');
+
+CREATE Clustered Index IX_DimEmployee_Name
+ON DimEmployee(EmployeeKey)
+
+CREATE Clustered Index IX_tblEmployee_Name
+ON tblEmployee(Name)
+  
+CREATE CLUSTERED INDEX IX_tblEmployee_Gender_Salary
+ON tblEmployee(Gender DESC, Salary ASC)
+
+CREATE NONCLUSTERED INDEX IX_tblEmployee_Name
+ON tblEmployee(Name)
+
+--Шестой файл
+
+CREATE TABLE [tblEmployee](
+id int Primary key,
+FirstName nvarchar(50),
+LatName nvarchar(50),
+Salary int,
+Gender nvarchar(10),
+City nvarchar(50));
+
+INSERT INTO tblEmployee
+Values(1,'Mike','Sandoz',4500,'Male','New York')
+INSERT INTO tblEmployee
+Values(1,'John','Menco',2500,'Male','London')
+--Мне выдало ошибку
+--Далее я удалил первичный УНИКАЛЬНЫЙ ключ
+-- и ввел эти же данные заново
+
+CREATE UNIQUE NONCLUSTERED INDEX UIX_tblEmployee_FirstName_LastName
+ON tblEmployee(FirstName,LatName)
+
+ALTER TABLE tblEmployee
+ADD CONSTRAINT UQ_tblEmployee_City
+UNIQUE NONCLUSTERED(City)
+
+CREATE UNIQUE INDEX IX_tblEmployee_City
+ON tblEmployee(City)
+WITH IGNORE_DUP_KEY
+
+-- Седьмой файл был выполнен по SELECT запросам поэтому в файле приложены только картинки.
